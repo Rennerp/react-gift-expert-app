060ea969414cfaafdb752e8182e5d6845d764c6e
Rennerp <rekefa@gmail.com>
2020-07-06 23:13:46 -0600

    docs

---
+++ docs/precache-manifest.e8749e3a4ad54f542ed98a36e1c01aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48161dcacd70dd1efed76ae2984c3f77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "399b5a16fa5792dff7c6",
+    "url": "/static/css/main.67cac742.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399b5a16fa5792dff7c6",
+    "url": "/static/js/main.30bdf9de.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
